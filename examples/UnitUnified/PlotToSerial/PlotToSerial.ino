ca318768dfb51872bcbad151ad763b14b6afed9c
GOB <goblin52@gmail.com>
2025-05-30 18:57:11 +0900

    Fixes compile error

--- examples/UnitUnified/PlotToSerial/PlotToSerial.ino
+++ examples/UnitUnified/PlotToSerial/PlotToSerial.ino
@@ -9,7 +9,7 @@
 // *************************************************************
 // Choose one define symbol to match the unit you are using
 // *************************************************************
-#if !defined(USING_UNIT_TOF) && !defined(USING_UNIT_TOF4M) && !defined(USING_HAT_TOF) !defined(USING_UNIT_TOF90)
+#if !defined(USING_UNIT_TOF) && !defined(USING_UNIT_TOF4M) && !defined(USING_HAT_TOF) && !defined(USING_UNIT_TOF90)
 // For UnitToF
 // #define USING_UNIT_TOF
 // For UnitToF4M
