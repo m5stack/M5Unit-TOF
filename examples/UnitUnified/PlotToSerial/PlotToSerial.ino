27de9c2df55b376477759687ebe1573dd01c438d
GOB <goblin52@gmail.com>
2025-05-30 18:41:58 +0900

    Add UnitToF90

--- examples/UnitUnified/PlotToSerial/PlotToSerial.ino
+++ examples/UnitUnified/PlotToSerial/PlotToSerial.ino
@@ -9,13 +9,15 @@
 // *************************************************************
 // Choose one define symbol to match the unit you are using
 // *************************************************************
-#if !defined(USING_UNIT_TOF) && !defined(USING_UNIT_TOF4M) && !defined(USING_HAT_TOF)
+#if !defined(USING_UNIT_TOF) && !defined(USING_UNIT_TOF4M) && !defined(USING_HAT_TOF) !defined(USING_UNIT_TOF90)
 // For UnitToF
 // #define USING_UNIT_TOF
 // For UnitToF4M
 // #define USING_UNIT_TOF4M
 // For HatToF
 // #define USING_HAT_TOF
+// For UnitToF90
+// #define USING_UNIT_TOF90
 #endif
 // *************************************************************
 #include "main/PlotToSerial.cpp"
